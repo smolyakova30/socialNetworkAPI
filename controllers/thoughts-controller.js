82aa5a659e17783c381872041e5a951e42be5964
Olga <o.smolyakova30@gmail.com>
2021-09-25 17:12:51 -0500

    added functions to gell all toughts, create, get thought by id

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -3,18 +3,50 @@ const {Thoughts, Users} = require('../models');
 const thoughtsController = {
 
     // get all available Thoughts
-    getAllThoughts() {
-      
+    getAllThoughts(req,res) {
+        Thoughts.find({})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        // .sort({_id: -1})
+        .then(dbThoughtsData => res.json(dbThoughtsData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     },
 
     // create thought
-    createThoughts(s) {
-         
+    createThoughts({params, body}, res) {
+        Thoughts.create(body)
+        .then(({_id}) => {
+            return Users.findOneAndUpdate({ _id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+        })
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({message: 'No thoughts with this particular ID!'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch(err => res.json(err)); 
     },
 
     // get thought by id
-    getThoughtsById() {
-        
+    getThoughtsById({params}, res) {
+        Thoughts.findOne({ _id: params.id })
+        .populate({path: 'reactions',select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+            res.status(404).json({message: 'No thoughts with this particular ID!'});
+            return;
+        }
+        res.json(dbThoughtsData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
     },
 
     // update thought by id
